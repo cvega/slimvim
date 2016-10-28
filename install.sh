af12c56b1679ddcf03839e2b636debc0ead2ab59
cvega <casey.vega@verizondigitalmedia.com>
2016-10-27 21:21:58 -0700

    update install.sh - point config to slimvim repo
    updated repo paths and name
    point to slimvim repo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/env sh
 
-INSTALLDIR=${INSTALLDIR:-"$PWD/vimified"}
+INSTALLDIR=${INSTALLDIR:-"$PWD/slimvim"}
 create_symlinks () {
     if [ ! -f ~/.vim ]; then
         echo "Now, we will create ~/.vim and ~/.vimrc files to configure Vim."
@@ -13,28 +13,28 @@ create_symlinks () {
   }
 
 echo "Welcome friend!"
-echo "You are about to be vimified. Ready? Let us do the stuff for you."
+echo "You are about to be slimvim. Ready? Let us do the stuff for you."
 
 which git > /dev/null
 if [ "$?" != "0" ]; then
-  echo "You need git installed to install vimified."
+  echo "You need git installed to install slimvim."
   exit 1
 fi
 
 which vim > /dev/null
 if [ "$?" != "0" ]; then
-  echo "You need vim installed to install vimified."
+  echo "You need vim installed to install slimvim."
   exit 1
 fi
 
 if [ ! -d "$INSTALLDIR" ]; then
-    echo "As we can't find Vimified in the current directory, we will create it."
-    git clone https://github.com/zaiste/vimified.git $INSTALLDIR
+    echo "As we can't find slimvim in the current directory, we will create it."
+    git clone https://github.com/cvega/slimvim.git $INSTALLDIR
     create_symlinks
     cd $INSTALLDIR
 
 else
-    echo "Seems like you already are one of ours, so let's update Vimified to be as awesome as possible."
+    echo "Seems like you already are one of ours, so let's update slimvim to be as awesome as possible."
     cd $INSTALLDIR
     git pull origin master
     create_symlinks
@@ -53,14 +53,14 @@ fi
 
 if [ ! -f local.vimrc ]; then
   echo "Let's create a 'local.vimrc' file so you have some bundles by default."
-  echo "let g:vimified_packages = ['general', 'fancy', 'css', 'js', 'os', 'html', 'coding', 'color']" > 'local.vimrc'
+  echo "let g:slimvim_packages = ['general', 'fancy', 'css', 'js', 'os', 'html', 'coding', 'color']" > 'local.vimrc'
 fi
 
 echo "There you are! Welcome in our world."
-echo "From now, do not hesitate to ask for help to the people behind Vimified: https://github.com/zaiste/vimified/graphs/contributors"
-echo "We welcome any bros/sistas who want to contribute: https://github.com/zaiste/vimified#call-for-help"
-echo "Report any issue/need: https://github.com/zaiste/vimified/issues"
-echo "At last, and before all, read the documentation: http://zaiste.github.com/vimified/"
+echo "From now, do not hesitate to ask for help to the people behind slimvim: https://github.com/cvega/slimvim/graphs/contributors"
+echo "We welcome any bros/sistas who want to contribute: https://github.com/cvega/slimvim#call-for-help"
+echo "Report any issue/need: https://github.com/cvega/slimvim/issues"
+echo "At last, and before all, read the documentation: http://cvega.github.com/slimvim/"
 
 echo "Enjoy!"
 
